6b9487e24cebfbbd013ecc741a7760ffd7326d37
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2024-02-25 20:36:49 +0100

    Cell class

---
+++ pyHojaDeCalculo/Utils/Cell.java
@@ -0,0 +1,17 @@
+package Utils;
+
+public class Cell {
+    private String cellValue;
+
+    public Cell(String cellValue) {
+        this.cellValue = cellValue;
+    }
+
+    public String getCellValue() {
+        return cellValue;
+    }
+
+    public void setCellValue(String cellValue) {
+        this.cellValue = cellValue;
+    }
+}
